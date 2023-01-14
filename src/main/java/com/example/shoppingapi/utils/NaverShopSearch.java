243648feb742b438ad51f6a87c4b8e4ead44c02a
Kim-Haul <bokugawa@naver.com>
2023-01-14 23:27:54 +0900

    :memo: ARC (Advanced REST Client)를 이용한 네이버 쇼핑 api 자바 (스프링) 코드 변환

---
+++ src/main/java/com/example/shoppingapi/utils/NaverShopSearch.java
@@ -0,0 +1,30 @@
+package com.example.shoppingapi.utils;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class NaverShopSearch {
+    public String search() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("X-Naver-Client-Id", "5HBxk7r1Ew8RhBvchPYK");
+        headers.add("X-Naver-Client-Secret", "OqT1Th_low");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=아이폰", HttpMethod.GET, requestEntity, String.class);
+        HttpStatusCode httpStatusCode = responseEntity.getStatusCode();
+        int status = httpStatusCode.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search();
+    }
+}
