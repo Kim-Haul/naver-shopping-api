29ecd699fd232bdc769dcdf4f65ad8626c3dfde8
Kim-Haul <bokugawa@naver.com>
2023-01-15 15:24:52 +0900

    :memo: Dto를 통해 Product 클래스를 만들 수 있게, Product 클래스 내부 코드 (생성자) 추가

--- src/main/java/com/example/shoppingapi/models/Product.java
+++ src/main/java/com/example/shoppingapi/models/Product.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
-public class Product extends Timestamped{
+public class Product extends Timestamped {
 
     // ID가 자동으로 생성 및 증가합니다.
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,4 +30,19 @@ public class Product extends Timestamped{
 
     @Column(nullable = false)
     private int myprice;
+
+    // 관심 상품 생성 시 이용합니다.
+    // 관심 상품을 등록하려면, 관심 상품 관련된 정보를 물고오는 Dto 가 전달이 될 것이고, 그걸 통해 Product 클래스를 만들면 Ok.
+    public Product(ProductRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.image = requestDto.getImage();
+        this.link = requestDto.getLink();
+        this.lprice = requestDto.getLprice();
+        this.myprice = 0;
+    }
+
+    // 관심 가격 변경 시 이용합니다.
+    public void update(ProductMypriceRequestDto requestDto) {
+        this.myprice = requestDto.getMyprice();
+    }
 }
