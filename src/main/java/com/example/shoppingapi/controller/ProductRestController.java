0be5f64ab0e795216d550a7bf3cc9f5fe0547212
Kim-Haul <bokugawa@naver.com>
2023-01-15 15:31:53 +0900

    :memo: PostMapping을 위한 controller 개선

--- src/main/java/com/example/shoppingapi/controller/ProductRestController.java
+++ src/main/java/com/example/shoppingapi/controller/ProductRestController.java
@@ -2,21 +2,34 @@ package com.example.shoppingapi.controller;
 
 import com.example.shoppingapi.models.Product;
 import com.example.shoppingapi.models.ProductRepository;
+import com.example.shoppingapi.models.ProductRequestDto;
+import com.example.shoppingapi.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequiredArgsConstructor // final 로 선언된 멤버 변수를 자동으로 생성합니다.
+@RequiredArgsConstructor // final로  선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON 으로 데이터를 주고받음을 선언합니다.
 public class ProductRestController {
 
+    private final ProductService productService;
     private final ProductRepository productRepository;
 
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    // 신규 상품 등록
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        productRepository.save(product);
+        return product;
     }
 }
