01ce61e6e700e15b41210bacc674f6d2b2bf4c85
Kim-Haul <bokugawa@naver.com>
2023-01-15 14:53:07 +0900

    :memo: Timestamped 클래스 생성 및 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/example/shoppingapi/ShoppingApiApplication.java
+++ src/main/java/com/example/shoppingapi/ShoppingApiApplication.java
@@ -2,7 +2,9 @@ package com.example.shoppingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA 에서 Timestamped 클래스 를 사용할 때, 시간 자동 변경이 가능하도록.
 @SpringBootApplication
 public class ShoppingApiApplication {
 
